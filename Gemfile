03bf55e6bcdbe563fcd79b172b1d0575841278b5
asqd <asqd117@gmail.com>
2014-05-21 18:04:55 +0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -17,7 +17,7 @@ group :test do
   gem 'capybara', '2.1.0'
   gem 'growl', '1.0.3'
 end
-
+gem 'bcrypt-ruby', '3.1.2'
 gem "bootstrap-sass", '2.3.2.0'
 gem 'sass-rails', '4.0.2'
 gem 'uglifier', '2.1.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,58 @@ describe "Static pages" do
 	
 	subject { page } 
 
+	shared_examples_for "all static pages" do
+		it { should have_selector('h1', text: heading) }
+		it { should have_title(full_title(page_title)) }
+	end
+
 	describe "Home page" do
 		before { visit root_path }
+		let(:heading) { 'Sample App' }
+		let(:page_title) { 'App' }
 
-		it { should have_content('Sample App') }
-		it { should have_title(full_title('App')) }				
+		it_should_behave_like "all static pages"
+		it { should_not have_title(full_title('Home')) }				
 	end
 
 	describe "Help page" do
 		before { visit help_path }
+		let(:heading) { 'Help' }
+		let(:page_title) { 'Help' }
 
-	    it { should have_content('Help') }
-	    it { should have_title(full_title('Help')) }
+		it_should_behave_like "all static pages"
   	end
 	
 	describe "About page" do
 		before { visit about_path }
+		let(:heading) { 'About' }
+		let(:page_title) { 'About' }
 
-		it { should have_content('About') }
-	    it { should have_title(full_title('About')) }
+		it_should_behave_like "all static pages"
 	end
 	 
 
 	describe "Contacts page" do
 		before { visit contacts_path }
 
-		it { should have_content('Contacts') }
-	    it { should have_title(full_title('Contacts')) }
+		let(:heading) { 'Contacts' }
+		let(:page_title) { 'Contacts' }
+
+		it_should_behave_like "all static pages"
+	end
+
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		expect(page).to have_title(full_title('About'))
+		click_link "Help"
+		expect(page).to have_title(full_title('Help'))
+		click_link "Contacts"
+		expect(page).to have_title(full_title('Contacts'))
+		click_link "Home"
+		click_link "Sign up now!"
+		expect(page).to have_title(full_title('Sign Up'))
+		click_link "sample app"
+		expect(page).to have_title(full_title('Tutorial'))   
 	end
 end
