9c6139f52bd1f6cba15314c00baec08a33870620
asqd <asqd117@gmail.com>
2014-06-02 21:19:25 +0400

    Add feed and destruction to user microposts

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,61 +1,77 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	
-	subject { page } 
 
-	shared_examples_for "all static pages" do
-		it { should have_selector('h1', text: heading) }
-		it { should have_title(full_title(page_title)) }
-	end
+  subject { page }
 
-	describe "Home page" do
-		before { visit root_path }
-		let(:heading) { 'Sample App' }
-		let(:page_title) { 'App' }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
-		it_should_behave_like "all static pages"
-		it { should_not have_title(full_title('Home')) }				
-	end
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { 'App' }
 
-	describe "Help page" do
-		before { visit help_path }
-		let(:heading) { 'Help' }
-		let(:page_title) { 'Help' }
+    it_should_behave_like "all static pages"
+    it { should_not have_title(full_title('Home')) }
 
-		it_should_behave_like "all static pages"
-  	end
-	
-	describe "About page" do
-		before { visit about_path }
-		let(:heading) { 'About' }
-		let(:page_title) { 'About' }
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        FactoryGirl.create(:micropost, user: user, content: "Immigrant Song")
+        FactoryGirl.create(:micropost, user: user, content: "Tenement Funster")
+        valid_signin user
+        visit root_path
+      end
 
-		it_should_behave_like "all static pages"
-	end
-	 
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          expect(page).to have_selector("li##{item.id}", text: item.content)
+        end
+      end
+    end
+  end
 
-	describe "Contacts page" do
-		before { visit contacts_path }
+  describe "Help page" do
+    before { visit help_path }
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
 
-		let(:heading) { 'Contacts' }
-		let(:page_title) { 'Contacts' }
+    it_should_behave_like "all static pages"
+  end
 
-		it_should_behave_like "all static pages"
-	end
+  describe "About page" do
+    before { visit about_path }
+    let(:heading) { 'About' }
+    let(:page_title) { 'About' }
 
-	it "should have the right links on the layout" do
-		visit root_path
-		click_link "About"
-		expect(page).to have_title(full_title('About'))
-		click_link "Help"
-		expect(page).to have_title(full_title('Help'))
-		click_link "Contacts"
-		expect(page).to have_title(full_title('Contacts'))
-		click_link "Home"
-		click_link "Sign up now!"
-		expect(page).to have_title(full_title('Sign Up'))
-		click_link "sample app"
-		expect(page).to have_title(full_title('Tutorial'))   
-	end
+    it_should_behave_like "all static pages"
+  end
+
+
+  describe "Contacts page" do
+    before { visit contacts_path }
+
+    let(:heading) { 'Contacts' }
+    let(:page_title) { 'Contacts' }
+
+    it_should_behave_like "all static pages"
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contacts"
+    expect(page).to have_title(full_title('Contacts'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign Up'))
+    click_link "sample app"
+    expect(page).to have_title(full_title('Tutorial'))
+  end
 end
