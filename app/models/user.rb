31fea7fec2bb16722f820cb350e7c5ac037e7df8
asqd <asqd117@gmail.com>
2014-06-03 03:33:48 +0400

    Add relationship models

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,12 @@ class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
   has_secure_password
 
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
   def User.new_remember_token
     SecureRandom.urlsafe_base64
   end
@@ -23,10 +29,22 @@ class User < ActiveRecord::Base
     Micropost.where("user_id = ?", id)
   end
 
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy!
+  end
+
   private
 
-  	def create_remember_token
-  		self.remember_token = User.encrypt(User.new_remember_token)  		
-  	end
+  def create_remember_token
+    self.remember_token = User.encrypt(User.new_remember_token)
+  end
 
 end
